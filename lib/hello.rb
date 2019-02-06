67f150637e4322920e754f6178b0367113c9c98a
Nicholas Dover <dovern42@gmail.com>
2019-02-06 00:30:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,13 @@
 def hello_t(array)
-  i = 0
-
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
-  array
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    puts "Put in a block, buster!"
 end
 
 # call your method here!
